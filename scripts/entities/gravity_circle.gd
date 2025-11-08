e26972ccf264a526a3737c27b5f4254172dc6800
Ryly Dou <rylydou@gmail.com>
2025-11-08 08:42:15 -0600

    wip planet system

--- scripts/entities/gravity_circle.gd
+++ scripts/entities/gravity_circle.gd
@@ -2,7 +2,13 @@
 class_name GravityZone extends Area2D
 
 
-@export var outer_radius := 64.0
+@export var radius := 64.0:
+	set(value):
+		radius = value
+		update()
+
+@export var graphic: Node2D
+@export var shape: CollisionShape2D
 
 
 func _ready() -> void:
@@ -10,4 +16,6 @@ func _ready() -> void:
 
 
 func update() -> void:
-	pass
+	if is_instance_valid(graphic) and is_instance_valid(shape):
+		graphic.scale = Vector2.ONE * radius * 2.0 * 1.2
+		shape.shape.radius = radius
--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,14 +1,21 @@
 class_name Player extends CharacterBody2D
 
 
+@export var camera: Camera2D
+
 @export_group("Jetpack", "jetpack_")
 
 @export var jetpack_boost_accel := 10.0
+@export var jetpack_max_speed := 100.0
 
 
 @export_group("Space Movement", "space_")
 
 @export var space_rotation_speed := 1.0
+## Will slightly nudge your velocity angle to your current angle over time
+@export var space_jetpack_steering_assist_over_time := 0.0
+## Will slightly nudge your velocity angle to your current angle when turning
+@export var space_jetpack_steering_assist_manual := 0.0
 
 @export var space_decel_linear := 0.0
 @export var space_decel_mult := 1.0
@@ -16,6 +23,7 @@ class_name Player extends CharacterBody2D
 
 @export_group("Planet Movement", "planet_")
 
+@export var planet_move_speed := 100.0
 @export var planet_max_move_speed := 100.0
 @export var planet_move_accel_air := 100.0
 @export var planet_move_accel_ground := 100.0
@@ -25,9 +33,6 @@ class_name Player extends CharacterBody2D
 @export var planet_decel_air_mult := 1.0
 @export var planet_decel_ground_linear := 0.0
 @export var planet_decel_ground_mult := 1.0
-
-@export var step_time := 0.2
-var step_timer := 0.0
 
 
 var gamepad := Gamepad.create(Gamepad.DEVICE_AUTO)
@@ -52,9 +57,6 @@ func _process_movement(delta: float) -> void:
 	if not gamepad.jump.down:
 		velocity += gravity * delta
 	
-	DevTools.sticky_toast(&"gravity", gravity)
-	DevTools.sticky_toast(&"move", gamepad.move)
-	
 	var has_gravity := not gravity.is_zero_approx()
 	
 	if has_gravity:
@@ -68,28 +70,40 @@ func _process_movement(delta: float) -> void:
 	if has_gravity:
 		_process_gravity(delta)
 	else:
-		vel_move = 0.0
 		_process_space(delta)
 	
-	var directed_move := Vector2.from_angle(rotation) * vel_move
-	velocity += directed_move
 	move_and_slide()
-	velocity -= directed_move
+	
+	var target_zoom := 2.0 if has_gravity else 1.0
+	
+	camera.zoom = Vector2.ONE * lerpf(camera.zoom.x, target_zoom, Math.smooth(1.0, delta))
 
 
 func _process_space(delta: float) -> void:
+	camera.rotation_smoothing_speed = 5.0
+	
+	var up := transform.basis_xform(Vector2.UP)
+	var right := transform.basis_xform(Vector2.RIGHT)
+	
 	is_jumping = false
 	rotation += gamepad.move.x * space_rotation_speed * delta
 	
-	if gamepad.jump.down:
+	if gamepad.jump.down and velocity.project(up).length() < jetpack_max_speed:
 		velocity += get_up_vector() * jetpack_boost_accel * delta
 	
 	velocity = velocity.move_toward(Vector2.ZERO, space_decel_linear * delta) * space_decel_mult
+	
+	# velocity = velocity
+	
+	var magnitude := velocity.length()
+	var new_angle := rotate_toward(velocity.angle(), up.angle(), (space_jetpack_steering_assist_over_time + absf(gamepad.move.x) * space_jetpack_steering_assist_manual) * delta)
+	
+	velocity = Vector2.from_angle(new_angle) * magnitude
 
-
-var vel_move := 0.0
 
 func _process_gravity(delta: float) -> void:
+	camera.rotation_smoothing_speed = 1.0
+	
 	var up := transform.basis_xform(Vector2.UP)
 	var right := transform.basis_xform(Vector2.RIGHT)
 	
@@ -103,37 +117,32 @@ func _process_gravity(delta: float) -> void:
 	var projection := velocity.project(right)
 	var current_move_speed := projection.length()
 	
-	if absf(current_move_speed) < absf(planet_max_move_speed):
+	if absf(current_move_speed) < absf(planet_move_speed):
 		velocity += right * gamepad.move.x * move_accel * delta
 	
 	# Deceleration if not moving
-	if is_zero_approx(gamepad.move.x) || signf(velocity.dot(right)) != signf(gamepad.move.x):
+	if (
+			absf(current_move_speed) > planet_max_move_speed
+			|| is_zero_approx(gamepad.move.x)
+			|| signf(velocity.dot(right)) != signf(gamepad.move.x)
+	):
 		var decel_linear = planet_decel_ground_linear if is_grounded else planet_decel_air_linear
 		var decel_mult = planet_decel_ground_mult if is_grounded else planet_decel_air_mult
 		
 		velocity = velocity.move_toward(Vector2.ZERO, decel_linear * delta) * decel_mult
 	
-	if vel_move == 0.0:
-		step_timer -= delta
-	
-	if is_grounded:
-		step_timer -= delta
-		if step_timer < 0.0:
-			step_timer = step_time
-			SFX.event(&"sfx/step").at(self).play()
-	
 	# Jumping and boosting
-	
 	if is_grounded and gamepad.jump.pressed:
 		is_jumping = true
 		velocity += get_up_vector() * planet_jump_velocity
 	
-	if gamepad.jump.down:
+	if gamepad.jump.down and velocity.project(up).length() < jetpack_max_speed:
 		velocity += get_up_vector() * jetpack_boost_accel * delta
 
 
 func _draw() -> void:
+	return
 	var gravity := get_gravity()
 	
-	draw_line(Vector2.ZERO, gravity.rotated(-rotation) * 300.0, Color.RED, 4.0)
+	draw_line(Vector2.ZERO, gravity.rotated(-rotation), Color.RED, 4.0)
 	draw_line(Vector2.ZERO, velocity.rotated(-rotation), Color.GREEN, 4.0)
